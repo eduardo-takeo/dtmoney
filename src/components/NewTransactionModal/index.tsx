3422bfd5db58ad22297706c84702ece41bd16092
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-05-12 20:55:34 -0300

    Add modal income/outcome buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Modal from "react-modal";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -30,6 +32,16 @@ export const NewTransactionModal = ({
         <h2>Cadastro de finanças</h2>
         <input type="text" placeholder="Nome" />
         <input type="text" placeholder="Preço" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Income" />
+            <span>Entradas</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Outcome" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
