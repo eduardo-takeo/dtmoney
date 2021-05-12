f3b4afc6d2e2745885d267ab120b9454323f0943
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-05-11 22:49:34 -0300

    Add fake API with MirageJs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { createServer } from "miragejs";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 import GlobalStyle from "./styles/global";
+
+createServer({
+  routes() {
+    this.namespace = "/api";
+
+    this.get("/transactions", () => [
+      { id: "1", name: "Luke" },
+      { id: "2", name: "Leia" },
+      { id: "3", name: "Anakin" },
+    ]);
+  },
+});
 
 function App() {
   return (
